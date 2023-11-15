59a168e8d249eab545638579ebbdc32ba3b9c799
Fluorky <mrdyrt.inf@gmail.com>
2023-11-15 19:11:57 +0100

    Update model

--- computer_service/models.py
+++ computer_service/models.py
@@ -14,23 +14,43 @@ class Person(models.Model):
     class Meta:
         abstract = True
 
-
 class ServiceRequest(CommonInfo):
-    # details would go here
-    pass
+    description = models.TextField()
+    requested_by = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    requested_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Service Request'
+        verbose_name_plural = 'Service Requests'
 
 class Invoice(ServiceRequest):
-     # details would go here
-     pass
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_status = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Invoice'
+        verbose_name_plural = 'Invoices'
 
 class Part(CommonInfo):
-    # details would go here
-    pass
+    description = models.CharField(max_length=100)
+    quantity_in_stock = models.PositiveIntegerField(default=0)
 
-class ServiceTechnicina(Person):
-    # details would go here
-    pass
+    class Meta:
+        verbose_name = 'Part'
+        verbose_name_plural = 'Parts'
+
+class ServiceTechnician(Person):
+    surname = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Service Technician'
+        verbose_name_plural = 'Service Technicians'
+
 class Customer(Person):
-    # details would go here
-    pass
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
 
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
