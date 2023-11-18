a5960829b7ecedd9d18f7cd31fb7708c29de5d17
Maciej B <mrdyrt.inf@gmail.com>
2023-11-18 21:23:20 +0100

    Update admin and tests

--- computer_service/admin.py
+++ computer_service/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import Part, ServiceRequest, ServiceTechnician, Invoice, Customer
+
 
 # Register your models here.
+
+admin.site.register(Part)
+admin.site.register(ServiceTechnician)
+admin.site.register(ServiceRequest)
+admin.site.register(Invoice)
+admin.site.register(Customer)
--- computer_service/tests.py
+++ computer_service/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Part
+
+class PartTestClass(TestCase):
+    def setUp(self):       
+        Part.objects.create(id=30, name="CPU I7 14700K", price=32, description = "INTEL CPU", quantity_in_stock = 10)
+    def test_1(self):
+        cpu = Part.objects.get(id=30)
+        self.assertEqual(cpu.quantity_in_stock,32)
+               
+    def test_2(self):
+        cpu = Part.objects.get(id=30)
+        self.assertEqual(cpu.price,32)
+
+##TO DO##
+# Creating tests here.
