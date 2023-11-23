1c36eb12747cf5dcf7231c19a1a7a04b0bb39dd9
Fluorky <mrdyrt.inf@gmail.com>
2023-11-23 06:19:00 +0100

    Add delete, detail, list a html templates to the customer views and correct paths to these htmls

--- computerserviceapp/views.py
+++ computerserviceapp/views.py
@@ -16,11 +16,11 @@ def index(request):
 
 def service_request_list(request):
     service_requests = ServiceRequest.objects.all()
-    return render(request, 'computerserviceapp/service_request_list.html', {'service_requests': service_requests})
+    return render(request, 'computerserviceapp/lists/service_request_list.html', {'service_requests': service_requests})
 
 def service_request_detail(request, pk):
     service_request = get_object_or_404(ServiceRequest, pk=pk)
-    return render(request, 'computerserviceapp/service_request_detail.html', {'service_request': service_request})
+    return render(request, 'computerserviceapp/details/service_request_detail.html', {'service_request': service_request})
 
 def service_request_create(request):
     if request.method == 'POST':
@@ -48,17 +48,17 @@ def service_request_delete(request, pk):
     if request.method == 'POST':
         service_request.delete()
         return redirect('service_request_list')
-    return render(request, 'computerserviceapp/service_request_confirm_delete.html', {'service_request': service_request})
+    return render(request, 'computerserviceapp/delete/service_request_confirm_delete.html', {'service_request': service_request})
 
 
 def invoice_list(request):
     invoices = Invoice.objects.all()
-    return render(request, 'computerserviceapp/invoice_list.html', {'invoices': invoices})
+    return render(request, 'computerserviceapp/lists/invoice_list.html', {'invoices': invoices})
 
 
 def invoice_detail(request, pk):
     invoice = get_object_or_404(Invoice, pk=pk)
-    return render(request, 'computerserviceapp/invoice_detail.html', {'invoice': invoice})
+    return render(request, 'computerserviceapp/details/invoice_detail.html', {'invoice': invoice})
 
 def invoice_create(request):
     if request.method == 'POST':
@@ -86,16 +86,16 @@ def invoice_delete(request, pk):
     if request.method == 'POST':
         invoice.delete()
         return redirect('invoice_list')
-    return render(request, 'computerserviceapp/invoice_confirm_delete.html', {'invoice': invoice})
+    return render(request, 'computerserviceapp/delete/invoice_confirm_delete.html', {'invoice': invoice})
 
 
 def part_list(request):
     parts = Part.objects.all()
-    return render(request, 'computerserviceapp/part_list.html', {'parts': parts})
+    return render(request, 'computerserviceapp/lists/part_list.html', {'parts': parts})
 
 def part_detail(request, pk):
     part = get_object_or_404(Part, pk=pk)
-    return render(request, 'computerserviceapp/part_detail.html', {'part': part})
+    return render(request, 'computerserviceapp/details/part_detail.html', {'part': part})
 
 def part_create(request):
     if request.method == 'POST':
@@ -123,16 +123,16 @@ def part_delete(request, pk):
     if request.method == 'POST':
         part.delete()
         return redirect('part_list')
-    return render(request, 'computerserviceapp/part_confirm_delete.html', {'part': part})
+    return render(request, 'computerserviceapp/delete/part_confirm_delete.html', {'part': part})
 
 
 def service_technician_list(request):
     service_technicians = ServiceTechnician.objects.all()
-    return render(request, 'computerserviceapp/service_technician_list.html', {'service_technicians': service_technicians})
+    return render(request, 'computerserviceapp/lists/service_technician_list.html', {'service_technicians': service_technicians})
 
 def service_technician_detail(request, pk):
     service_technician = get_object_or_404(ServiceTechnician, pk=pk)
-    return render(request, 'computerserviceapp/service_technician_detail.html', {'service_technician': service_technician})
+    return render(request, 'computerserviceapp/details/service_technician_detail.html', {'service_technician': service_technician})
 
 def service_technician_create(request):
     if request.method == 'POST':
@@ -160,16 +160,16 @@ def service_technician_delete(request, pk):
     if request.method == 'POST':
         service_technician.delete()
         return redirect('ServiceTechnician_list')
-    return render(request, 'computerserviceapp/service_technician_confirm_delete.html', {'service_technician': service_technician})
+    return render(request, 'computerserviceapp/delete/service_technician_confirm_delete.html', {'service_technician': service_technician})
 
 
 def customer_list(request):
     customers = Customer.objects.all()
-    return render(request, 'computerserviceapp/customer_list.html', {'customers': customers})
+    return render(request, 'computerserviceapp/lists/customer_list.html', {'customers': customers})
 
 def customer_detail(request, pk):
     customer = get_object_or_404(Customer, pk=pk)
-    return render(request, 'computerserviceapp/customer_detail.html', {'customer': customer})
+    return render(request, 'computerserviceapp/details/customer_detail.html', {'customer': customer})
 
 def customer_create(request):
     if request.method == 'POST':
@@ -197,5 +197,5 @@ def customer_delete(request, pk):
     if request.method == 'POST':
         customer.delete()
         return redirect('customer_list')
-    return render(request, 'computerserviceapp/customer_confirm_delete.html', {'customer': customer})
+    return render(request, 'computerserviceapp/delete/customer_confirm_delete.html', {'customer': customer})
 ##TO DO##
